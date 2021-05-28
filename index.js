410d36d8a22a27b5b4e206fcecb644bc7aa625ef
mochi5o <1610.baumkuchen.124@gmail.com>
2021-05-28 18:31:37 +0900

    変数を使わないでテスト

--- index.js
+++ index.js
@@ -5,77 +5,50 @@ const notionToken = core.getInput('integrations-token');
 const notion = new Client({
   auth: notionToken,
   logLevel: LogLevel.DEBUG,
-})
-// async function run() {
-//   try {
-//     const notionToken = core.getInput('integrations-token');
-//     const notion = new Client({
-//       auth: notionToken,
-//       logLevel: LogLevel.DEBUG,
-//     })
-//     const issueTitle = core.getInput('issue-title');
-//     const url = core.getInput('url');
+});
+const issueTitle = core.getInput('issue-title');
+const url = core.getInput('url');
 
-//     const parent = {
-//           database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a'
-//         };
+async function run() {
+  try {
 
-//     const properties = {
-//       'Name': {
-//         type: 'title',
-//         title: [{ text: { content: issueTitle } }]
-//       },
-//       'Url': {
-//         type: 'rich_text',
-//         rich_text: [{ text: { content: url } }]
-//       },
-//       'Status': {
-//         select: {
-//           name: "not started"
-//         }
-//       }
-//     }
+    const parent = {
+          database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a'
+        };
 
-//     const newPage = await notion.pages.create({
-//       parent: {
-//         database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a',
-//       },
-//       propaties: properties
-//     })
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
-
-// run()
-(async () => {
-  const response = await notion.pages.create({
-    parent: {
-      database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a',
-    },
-    properties: {
-      'Name': {
-        type: 'title',
-        title: [
-          {
-            type: 'text',
-            text: {
-              content: 'Tomatoes',
-            },
-          },
-        ],
+    const properties = {
+      Name: {
+        type: "title",
+        title: [{ "text": { "content": "POST Data" } }]
+      },
+      Tags: {
+        type: "rich_text",
+        rich_text: [{ "text": { "content": "actions" } }]
       },
       Price: {
-        type: 'number',
-        number: 1.49,
+        type: "number",
+        number: 500
       },
-      'Date': {
-        type: 'date',
-        date: {
-          start: '2021-05-11',
-        },
+      Date: {
+        type: "date",
+        date: { "start": "2021-05-11" }
       },
-    },
-  });
-  console.log(response);
-})();
+      Status: {
+        select: {
+          name: "complete"
+        }
+      }
+    }
+
+    const newPage = await notion.pages.create({
+      parent: {
+        database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a',
+      },
+      propaties: properties
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+run()
