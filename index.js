6baf3df20a3e6f0db2bae5ddb4f7b3aa14b07a71
mochi5o <1610.baumkuchen.124@gmail.com>
2021-05-28 16:55:38 +0900

    sampleコード

--- index.js
+++ index.js
@@ -2,45 +2,76 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { Client, LogLevel } = require("@notionhq/client")
 
-async function run() {
-  try {
-    const notionToken = core.getInput('integrations-token');
-    const notion = new Client({
-      auth: notionToken,
-      logLevel: LogLevel.DEBUG,
-    })
-    const issueTitle = core.getInput('issue-title');
-    const url = core.getInput('url');
+// async function run() {
+//   try {
+//     const notionToken = core.getInput('integrations-token');
+//     const notion = new Client({
+//       auth: notionToken,
+//       logLevel: LogLevel.DEBUG,
+//     })
+//     const issueTitle = core.getInput('issue-title');
+//     const url = core.getInput('url');
 
-    const parent = {
-          database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a'
-        };
+//     const parent = {
+//           database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a'
+//         };
 
-    const properties = {
+//     const properties = {
+//       'Name': {
+//         type: 'title',
+//         title: [{ text: { content: issueTitle } }]
+//       },
+//       'Url': {
+//         type: 'rich_text',
+//         rich_text: [{ text: { content: url } }]
+//       },
+//       'Status': {
+//         select: {
+//           name: "not started"
+//         }
+//       }
+//     }
+
+//     const newPage = await notion.pages.create({
+//       parent: {
+//         database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a',
+//       },
+//       propaties: properties
+//     })
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+// run()
+(async () => {
+  const response = await notion.pages.create({
+    parent: {
+      database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a',
+    },
+    properties: {
       'Name': {
         type: 'title',
-        title: [{ text: { content: issueTitle } }]
+        title: [
+          {
+            type: 'text',
+            text: {
+              content: 'Tomatoes',
+            },
+          },
+        ],
       },
-      'Url': {
-        type: 'rich_text',
-        rich_text: [{ text: { content: url } }]
+      Price: {
+        type: 'number',
+        number: 1.49,
       },
-      'Status': {
-        select: {
-          name: "not started"
-        }
-      }
-    }
-
-    const newPage = await notion.pages.create({
-      parent: {
-        database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a',
+      'Date': {
+        type: 'date',
+        date: {
+          start: '2021-05-11',
+        },
       },
-      propaties: properties
-    })
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-run()
+    },
+  });
+  console.log(response);
+})();
