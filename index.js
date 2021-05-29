64d0167083a8e9ca00ce75fda9b1754c640a9f72
mochi5o <1610.baumkuchen.124@gmail.com>
2021-05-29 20:41:08 +0900

    変数名を変えてテスト

--- index.js
+++ index.js
@@ -5,40 +5,26 @@ const { Client, LogLevel } = require('@notionhq/client')
 async function run() {
   try {
 
-    // const parent = {
-    //   database_id: ''
-    // }
+    const parentData = {
+      database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a'
+    }
 
-    // const properties = {
-    //   Name: {
-    //     type: 'title',
-    //     title: [{ text: { content: 'POST Data' } }]
-    //   },
-    //   Tags: {
-    //     type: 'rich_text',
-    //     rich_text: [{ text: { content: 'actions' } }]
-    //   },
-    //   Price: {
-    //     type: 'number',
-    //     number: 500
-    //   },
-    //   Date: {
-    //     type: 'date',
-    //     date: { start: '2021-05-11' }
-    //   },
-    //   Status: {
-    //     type: 'select',
-    //     select: {
-    //       name: 'complete'
-    //     }
-    //   }
-    // }
-    // const val = {parent: {
-    //     page_id: '48f8fee9cd794180bc2fec0398253067',
-    //   },
-    //   properties: properties
-    // }
-    // console.log(val);
+    const prop = {
+      Name: {
+        type: 'title',
+        title: [{ text: { content: issueTitle } }]
+      },
+      Url: {
+        type: 'rich_text',
+        rich_text: [{ text: { content: url } }]
+      },
+      Status: {
+        select: {
+          name: "not started"
+        }
+      }
+    }
+
     const notionToken = core.getInput('integrations-token');
     const notion = new Client({
       auth: notionToken,
@@ -47,28 +33,12 @@ async function run() {
     const issueTitle = core.getInput('issue-title');
     const url = core.getInput('url');
     const newPage = await notion.pages.create({
-      parent: {
-        database_id: 'dfc2cdc260ae476c8f2f5d4065f9a94a',
-      },
-      properties: {
-        Name: {
-          type: 'title',
-          title: [{ text: { content: issueTitle } }]
-        },
-        Url: {
-          type: 'rich_text',
-          rich_text: [{ text: { content: url } }]
-        },
-        Status: {
-          select: {
-            name: "not started"
-          }
-        }
-      }
+      parent: parentData,
+      properties: prop
     })
     console.log(newPage);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 run()
