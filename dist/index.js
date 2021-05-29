6e4862eade8bb16c9d938887d6af09738ad92083
mochi5o <1610.baumkuchen.124@gmail.com>
2021-05-29 19:39:41 +0900

    typo

--- dist/index.js
+++ dist/index.js
@@ -1210,7 +1210,7 @@ async function run() {
     const val = {parent: {
         page_id: '48f8fee9cd794180bc2fec0398253067',
       },
-      propaties: properties
+      properties: properties
     }
     console.log(val);
 
@@ -1219,7 +1219,7 @@ async function run() {
         database_id: '48f8fee9cd794180bc2fec0398253067',
         page_id: '48f8fee9cd794180bc2fec0398253067'
       },
-      propaties: properties
+      properties: properties
     })
     console.log(newPage);
   } catch (error) {
