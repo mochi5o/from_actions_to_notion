78e6a11646a070aed531de13152f495c5e7bf88d
mochi5o <1610.baumkuchen.124@gmail.com>
2021-06-01 12:20:36 +0900

    とりあえずprettierとlinterを入れてみた

--- main.js
+++ main.js
@@ -1,40 +1,41 @@
 const core = require('@actions/core');
-const { Client, LogLevel } = require('@notionhq/client')
+const { Client, LogLevel } = require('@notionhq/client');
+
+const notionToken = core.getInput('integrations-token');
+const issueTitle = core.getInput('issue-title');
+const url = core.getInput('url');
+const dbId = core.getInput('db-id');
+const notion = new Client({
+  auth: notionToken,
+  logLevel: LogLevel.DEBUG,
+});
 
 const main = async () => {
   try {
-    const notionToken = core.getInput('integrations-token');
-    const notion = new Client({
-      auth: notionToken,
-      logLevel: LogLevel.DEBUG,
-    });
-    const issueTitle = core.getInput('issue-title');
-    const url = core.getInput('url');
-    const dbId = core.getInput('db-id')
     const parent = {
-      database_id: dbId
-    }
+      database_id: dbId,
+    };
     const properties = {
       Title: {
-        title: [{ text: { content: issueTitle } }]
+        title: [{ text: { content: issueTitle } }],
       },
       Url: {
-        url: url
+        url: url,
       },
       Status: {
         select: {
-          name: "not started"
-        }
-      }
-    }
+          name: 'not started',
+        },
+      },
+    };
     const newPage = await notion.pages.create({
       parent: parent,
-      properties: properties
-    })
-    console.log(newPage);
+      properties: properties,
+    });
+    core.setOutput('new-page', newPage);
   } catch (error) {
-    console.error(error);
+    core.setFailed(error.message);
   }
-}
+};
 
 module.exports = main;
